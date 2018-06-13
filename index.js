c8c98031259581b425ec9ae122e8aefdc56be503
Akumimi <40242902+Akumimi@users.noreply.github.com>
2018-06-13 18:06:19 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    bot.user.setGame("Senpai");
+    console.log("Akumi est bien connecté :3");
+});
+
+bot.login("NDU2NDYwOTg5NTk2OTU4NzMx.DgLOpA.qahlVJmWxdppHSiOiUiqojTRDuM");
